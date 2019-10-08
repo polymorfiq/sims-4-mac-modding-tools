47d4fb8b6065b455f27cc1487cb217f2fc201641
Cory Finger <cory.j.finger@gmail.com>
2019-10-07 21:58:16 -0700

    Finish up basic version of drones

--- Mods/uncensored_drones/uncensored_drones.py
+++ Mods/uncensored_drones/uncensored_drones.py
@@ -1,28 +1,7 @@
-import services
-import sims4.commands
-from weakref import WeakSet
+from interactions.privacy import PrivacyService
 from s4mmtlib.game_injector import GameInjector
-from s4mmtlib.mod_logger import logger
-from routing.object_routing.object_routing_component import ObjectRoutingComponent
-from interactions.privacy import PrivacyService, Privacy
-from objects.components.privacy_component import PrivacyComponent
-from objects.components.idle_component import IdleComponent
-from objects.game_object import GameObject
-from turbolib2.events.privacy import _TurboPrivacyUtil
-from turbolib2.events.utils.events_handler import TurboEventsHandler
-from turbolib2.wrappers.line_of_sight import _SuperTurboLineOfSight
 
-@sims4.commands.Command('uncensor_drones', command_type=sims4.commands.CommandType.Live)
-@logger
-def uncensor_drones(_connection=None):
-    client_id = services.client_manager().get_first_client_id()
-    output = sims4.commands.CheatOutput(client_id)
-
-    connect_mod()
-    output("Mod Injected!")
-
-@logger
-def connect_mod(_connection=None):
+def connect_mod():
     injector = GameInjector()
 
     injector.wrap_function(PrivacyService, 'add_vehicle_to_monitor', observe_key='add_vehicle_override')
@@ -32,14 +11,8 @@ def connect_mod(_connection=None):
     injector.before('remove_vehicle_override', disable_vehicle_call)
 
 
-@logger
 def disable_vehicle_call(obs, val, ctrl):
     if len(ctrl.args) > 1 and ("Drone" in str(ctrl.args[1])):
         ctrl.do_skip_call()
 
-
-@logger
-def watch_call(obs, val, ctrl):
-    client_id = services.client_manager().get_first_client_id()
-    output = sims4.commands.CheatOutput(client_id)
-    output(obs.fnc_name + " called! - " + str(ctrl.args))
+connect_mod()
